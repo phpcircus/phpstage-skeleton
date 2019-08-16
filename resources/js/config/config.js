39a3698197c00b53627939b608140b3f0f9d80d9
Clayton Stone <devcircus@outlook.com>
2019-08-15 23:36:47 -0500

    Fix some environment variables.

--- resources/js/config/config.js
+++ resources/js/config/config.js
@@ -1,7 +1,7 @@
 export const config = {
     apiUrl: process.env.MIX_API_URL,
     appName: process.env.MIX_APP_NAME,
-    timezone: process.env.MIX_FRONTEND_TIMEZONE,
+    timezone: process.env.MIX_APP_TIMEZONE,
     pusherKey: process.env.MIX_PUSHER_APP_KEY,
     pusherCluster: process.env.MIX_PUSHER_APP_CLUSTER,
     admin: {
